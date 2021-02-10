7752ade56e702f1b07a777d4184adbe9fe8b3096
StassyaS <nastushaaa@i.ua>
2021-02-10 21:16:39 +0200

    sec change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav.jsx'
+import Nav from './components/Nav'
 
 
 
--- my-app/src/components/Nav.jsx
+++ my-app/src/components/Nav.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import './Nav.css';
-import Api from './components/Api.jsx'
-import Game from './components/Game.jsx'
-import Calculator from './components/Calculator.jsx'
+import Api from './components/Api'
+import Game from './components/Game'
+import Calculator from './components/Calculator'
 
 
 const Nav = () => {
-  return <nav className='nav'>
-    <ol>
-      <li>
-        <a href="">
-          <Game />
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <Calculator />
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <Api /> 
-        </a>
-      </li>
-    </ol>
-  </nav>
+  return (
+    <nav className='nav'>
+      <ol>
+        <li>
+          <a href="">
+            <Game />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <Calculator />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <Api /> 
+          </a>
+        </li>
+      </ol>
+    </nav>    
+  )
+
 }
 
 export default Nav ;
